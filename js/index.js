57d7b058829f5fd5e1b1b1f7e473d18b9b2a7381
Carlos Hernández Flórez <carloshflorez.web@gmail.com>
2023-04-05 00:40:44 +0200

    Slider

---
+++ js/index.js
@@ -0,0 +1,68 @@
+`use strict`;
+
+const galeriaCont = document.querySelector(`.Galeria-container`);
+const arrowNext = document.querySelector(`.next`);
+const arrowPrev = document.querySelector(`.prev`);
+const galeriaImg = document.querySelectorAll(`.Galeria-img`);
+const galeriaBtn = document.querySelectorAll(`.Galeria-btn`);
+
+let contador = 0;
+
+galeriaBtn[contador].classList.add(`isActive`);
+console.log(contador);
+
+arrowNext.addEventListener(`click`, () => {
+  contador++;
+  let operacion = contador * -(100 / 6);
+  galeriaCont.style.transform = `translateX(${operacion}%)`;
+  console.log(contador);
+  if (contador >= 6) {
+    contador = 0;
+    galeriaCont.style.transform = `translateX(${operacion + 100}%)`;
+    console.log(contador);
+  }
+  galeriaImg.forEach((eachImg, i) => {
+    galeriaBtn[i].classList.remove(`isActive`);
+  });
+  galeriaBtn[contador].classList.add(`isActive`);
+});
+
+arrowPrev.addEventListener(`click`, () => {
+  contador--;
+  let operacion = contador * -(100 / 6);
+  galeriaCont.style.transform = `translateX(${operacion}%)`;
+  console.log(contador);
+  if (contador <= -1) {
+    contador = 5;
+    galeriaCont.style.transform = `translateX(${operacion - 100}%)`;
+    console.log(contador);
+  }
+  galeriaImg.forEach((eachImg, i) => {
+    galeriaBtn[i].classList.remove(`isActive`);
+  });
+  galeriaBtn[contador].classList.add(`isActive`);
+});
+
+galeriaBtn.forEach((eachBtn, i) => {
+  galeriaBtn[i].addEventListener(`click`, () => {
+    contador = i;
+    let operacion = contador * -(100 / 6);
+    galeriaBtn.forEach((eachBtn, i) => {
+      galeriaBtn[i].classList.remove(`isActive`);
+    });
+    galeriaBtn[i].classList.add(`isActive`);
+    galeriaCont.style.transform = `translateX(${operacion}%)`;
+  });
+});
+if ((contador = 5)) {
+  const fragmento = document.createDocumentFragment();
+  const firstImg = document.createElement(`img`);
+  firstImg.classList.add(`Galeria-img`);
+  firstImg.setAttribute(`src`, `assets/img/posada-portrait.jpg`);
+  firstImg.setAttribute(`alt`, `Posada2`);
+
+  fragmento.appendChild(firstImg);
+
+  const elementImg = document.getElementsByClassName(`Galeria-container`);
+  elementImg.appendChild(fragmento);
+}
