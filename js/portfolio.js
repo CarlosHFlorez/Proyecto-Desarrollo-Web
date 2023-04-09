8fa874e9a3c624ed605557ed279988a50cfa1695
Carlos Hernández Flórez <carloshflorez.web@gmail.com>
2023-04-09 12:35:35 +0200

    Ajustes en portfolio.html, css y js

---
+++ js/portfolio.js
@@ -0,0 +1,10 @@
+//SCRIPT PARA EL BOTON HAMBURGUESA EN EL MODO MÓVIL
+
+//Seleccionamos los objetos implicados
+const headerBtn = document.querySelector(`.Header-btn`);
+const headerResp = document.querySelector(`.Header-responsive`);
+
+//Al hacer click en Header-btn hacemos toggle de la clase isActive para la etiqueta Header-responsive
+headerBtn.addEventListener(`click`, () => {
+  headerResp.classList.toggle(`isActive`);
+});
