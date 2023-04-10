febd7b406e30bc9ab1789d5b309426f8d064ff54
Carlos Hernández Flórez <carloshflorez.web@gmail.com>
2023-04-10 02:42:00 +0200

    Lightbox en portfolio.css
    Falta hacer el responsive

--- js/portfolio.js
+++ js/portfolio.js
@@ -8,3 +8,40 @@ const headerResp = document.querySelector(`.Header-responsive`);
 headerBtn.addEventListener(`click`, () => {
   headerResp.classList.toggle(`isActive`);
 });
+
+//SCRIPT PARA EL LIGHTBOX EN PROYECTOS
+
+//Seleccionamos los objetos implicados
+const proyectosImg = document.querySelectorAll(`.Proyectos-img`);
+const galeriaImg = document.querySelectorAll(`.Galeria-img`);
+const lightboxImg = document.querySelector(`.Lightbox-img`);
+const lightboxArticle = document.querySelectorAll(`.Lightbox-article`);
+const lightbox = document.querySelector(`.Lightbox`);
+const lightboxBtn = document.querySelector(`.Lightbox-btn`);
+const titlesH2 = document.querySelectorAll(`.Title-h2`);
+const lightboxH2 = document.querySelector(`.Lightbox-h2`);
+const softwareSpan = document.querySelectorAll(`.Software-span`);
+const lightboxSpanProgramas = document.querySelector(`.Lightbox-span-programas`);
+const extractoP = document.querySelectorAll(`.Extracto-p`);
+const lightboxP = document.querySelector(`.Lightbox-p`);
+
+//Recorremos el array de .Proyectios-section, al hacer click en alguna de las etiquetas .Proyectos-section le añadimos a .Lightbox la clase isActive
+proyectosImg.forEach((eachImg, i) => {
+  proyectosImg[i].addEventListener(`click`, () => {
+    lightbox.classList.add(`isActive`);
+    let source = galeriaImg[i].getAttribute(`src`);
+    lightboxImg.setAttribute(`src`, source);
+    let titulo = titlesH2[i].textContent;
+    lightboxH2.textContent = titulo;
+    let programas = softwareSpan[i].textContent;
+    lightboxSpanProgramas.textContent = programas;
+    let texto = extractoP[i].textContent;
+    lightboxP.textContent = texto;
+  });
+  //Al hacer click en la etiqueta .Lightbox-btn le quitamos la clase isActive a .Lightbox
+  lightboxBtn.addEventListener(`click`, () => {
+    lightbox.classList.remove(`isActive`);
+    lightboxImg.classList.remove(`isActive`);
+    lightboxP.classList.remove(`isActive`);
+  });
+});
