d787e5b309f32be111b1fd961f243f3be054e68b
Carlos Hernández Flórez <carloshflorez.web@gmail.com>
2023-04-13 00:32:08 +0200

    Mejora del script para al efecto CHF

--- js/about.js
+++ js/about.js
@@ -1,10 +1,10 @@
 //Al cargar la página About le quitamos la clase hidden a los ul
 
-const textEffect = document.querySelector(`.Texto-ul.shown.hidden`);
+const textEffect = document.querySelector(`.Texto-ul.isActive`);
 
 textEffect.addEventListener(`mouseover`, () => {
-  textEffect.classList.remove(`hidden`);
+  textEffect.classList.remove(`isActive`);
 });
 textEffect.addEventListener(`mouseout`, () => {
-  textEffect.classList.add(`hidden`);
+  textEffect.classList.add(`isActive`);
 });
