97712fa380b9a487aea989e4004495c2cdcfe20d
Carlos Hernández Flórez <carloshflorez.web@gmail.com>
2023-04-13 18:43:39 +0200

    Responside en about.html

--- js/about.js
+++ js/about.js
@@ -1,3 +1,14 @@
+//SCRIPT PARA EL BOTON HAMBURGUESA EN EL MODO MÓVIL
+
+//Seleccionamos los objetos implicados
+const headerBtn = document.querySelector(`.Header-btn`);
+const headerResp = document.querySelector(`.Header-responsive`);
+
+//Al hacer click en Header-btn hacemos toggle de la clase isActive para la etiqueta Header-responsive
+headerBtn.addEventListener(`click`, () => {
+  headerResp.classList.toggle(`isActive`);
+});
+
 //Al cargar la página About le quitamos la clase hidden a los ul
 
 const textEffect = document.querySelector(`.Texto-ul`);
